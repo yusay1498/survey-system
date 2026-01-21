124859039d62d321a942dfb5f702d267ad272fce
yusay1498 <u.yusay1498@gmail.com>
2026-01-21 21:47:12 +0900

    add questionnaire entities

---
+++ src/entities/answer.ts
@@ -0,0 +1,8 @@
+export type Answer = {
+  id?: string;
+  userId: string;
+  userName: string;
+  questionId: string;
+  selectedOption: string;
+  createdAt: Date;
+};
---
+++ src/entities/question.ts
@@ -0,0 +1,6 @@
+export type Question = {
+  id: string;
+  text: string;
+  options: string[];
+  order: number;
+};
